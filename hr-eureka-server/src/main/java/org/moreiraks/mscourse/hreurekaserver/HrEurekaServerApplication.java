5c769845ba77f00d946ae7d9d4026ad1cdf818d6
Kaique Moreira da Silva <kaique.moreira.dev@gmail.com>
2023-04-01 10:52:44 -0300

    feat(hr-eureka-server): configured

--- hr-eureka-server/src/main/java/org/moreiraks/mscourse/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/org/moreiraks/mscourse/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,10 @@ package org.moreiraks.mscourse.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
