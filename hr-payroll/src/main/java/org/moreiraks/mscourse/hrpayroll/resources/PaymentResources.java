2db243dc3f0fbfa0f8426b89b6ef59a0d9851d55
Kaique Moreira da Silva <kaique.moreira.dev@gmail.com>
2023-04-03 17:43:06 -0300

    feat(hr-worker): configured hystrix fault tolerance

--- hr-payroll/src/main/java/org/moreiraks/mscourse/hrpayroll/resources/PaymentResources.java
+++ hr-payroll/src/main/java/org/moreiraks/mscourse/hrpayroll/resources/PaymentResources.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/payments")
 public class PaymentResources {
@@ -20,8 +22,13 @@ public class PaymentResources {
   }
 
 
+  @HystrixCommand(fallbackMethod = "getPaymentAlternative")
   @GetMapping("/{workerId}/days/{days}")
   public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
     return ResponseEntity.ok(paymentService.getPayment(workerId, days));
+  }
+
+  public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+    return ResponseEntity.ok(new Payment("test", 200.0, days));
   }
 }
